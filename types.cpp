09762387753247024f884a3b4586eacbf0dc25d7
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-06-17 12:26:18 +0200

    added file with types

---
+++ types.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+
+	std::cout <<"size of int = " << sizeof(int) << std::endl;
+
+	std::cout <<"size of short = " << sizeof(short) << std::endl;
+
+	std::cout <<"size of long = " << sizeof(long) << std::endl;
+
+	std::cout <<"size of long long = " << sizeof(long long) << std::endl;
+
+	std::cout <<"size of char = " << sizeof(char) << std::endl;
+	
+	return 0;
+}
