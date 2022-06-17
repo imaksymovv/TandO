0a9dd7bee0306e6c94c0a57f08f0c86d77b05aa6
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-06-17 12:25:16 +0200

    added file with operands

---
+++ operators.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	int c, result;
+	std::cout << "enter the number of your operand(& = 1, ^ = 2, | = 3, ~ = 4):" << std::endl;
+	std::cin >> c;
+	if (c == 1) {
+		result = a & b;
+		std::cout << "Result = "<< result << std::endl;
+	}
+	else if(c == 2){
+		result = a ^ b;
+		std::cout << "Result = " << result << std::endl;
+	}
+	else if (c == 3) {
+		result = a | b;
+		std::cout << "Result = " << result << std::endl;
+	}
+	else if (c == 4) {
+		result = ~(a + b);
+		std::cout << "Result = " << result << std::endl;
+	}
+
+	return 0;
+}
+
